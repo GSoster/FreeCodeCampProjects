7a28d40d0ba15377c84c9bfc314d35b2a60a0ab9
Guilherme Soster <GSoster@users.noreply.github.com>
2018-05-16 08:24:33 -0300

    added initial files

---
+++ show_the_local_weather/script.js
@@ -0,0 +1,30 @@
+const endpoint = 'https://fcc-weather-api.glitch.me/api/current?';
+
+if (navigator.geolocation)
+   position = navigator.geolocation.getCurrentPosition(fetchInfoAtPosition);
+else
+  console.log("doesn't have navigator");
+
+
+
+function fetchInfoAtPosition (position) 
+{  
+  let [lat, long] = [position.coords.latitude, position.coords.longitude];
+  
+  fetch(endpoint + `lat=${lat}&lon=${long}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(jsonData => {
+      console.log(jsonData);
+      DisplayWeatherData(jsonData);
+     });  
+}
+
+
+function DisplayWeatherData (weatherData)
+{
+  document.querySelector('#temperature').innerText = weatherData.main.temp;
+  document.querySelector('#min-temperature').innerText = weatherData.main.temp_min;
+  document.querySelector('#max-temperature').innerText = weatherData.main.temp_max;
+}
